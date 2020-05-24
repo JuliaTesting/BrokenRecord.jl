211e90817b8f9973b489baa37df7e97f68a113e0
Chris de Graaf <me@cdg.dev>
2020-05-24 00:49:39 -0500

    Initial version

--- src/HTTPPlayback.jl
+++ src/HTTPPlayback.jl
@@ -1,5 +1,32 @@
 module HTTPPlayback
 
-# Write your package code here.
+using BSON: bson, load
+using Cassette: Cassette, disablehooks, overdub, @context
+using HTTP: HTTP, request
+
+@context RecordingCtx
+Cassette.posthook(ctx::RecordingCtx, resp, ::typeof(request), ::Type{Union{}}, args...) =
+    push!(ctx.metadata, resp)
+function after(ctx::RecordingCtx, path)
+    mkpath(dirname(path))
+    bson(path; responses=ctx.metadata)
+end
+
+@context PlaybackCtx
+Cassette.overdub(ctx::PlaybackCtx, ::typeof(request), args...) = popfirst!(ctx.metadata)
+after(ctx::PlaybackCtx, path) = nothing
+
+function playback(f, path; transform=identity)
+    ctx = if isfile(path)
+        data = load(path)
+        disablehooks(PlaybackCtx(; metadata=data[:responses]))
+    else
+        RecordingCtx(; metadata=[])
+    end
+
+    result = overdub(ctx, f)
+    after(ctx, path)
+    return result
+end
 
 end
