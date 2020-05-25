6ae9f29ab10078fdc8df0e0aaba58594874f3f1e
Chris de Graaf <me@cdg.dev>
2020-05-25 11:06:53 -0500

    Add basic test cases

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,22 @@
-using HTTPPlayback
-using Test
+using Test: @test, @testset
+
+using BSON: load
+using HTTP: HTTP
+
+using HTTPPlayback: playback
 
 @testset "HTTPPlayback.jl" begin
-    # Write your tests here.
+    mktempdir() do dir
+        path = joinpath(dir, "test1.bson")
+        @test playback(() -> 1, path) == 1
+        @test isfile(path)
+        @test load(path) == Dict(:responses => [])
+
+        path = joinpath(dir, "test2.bson")
+        @test !isfile(path)
+        resp1 = playback(() -> HTTP.get("https://httpbin.org/get"), path)
+        @test isfile(path)
+        resp2 = playback(() -> HTTP.get("https://httpbin.org/get"), path)
+        @test resp1.body == resp2.body
+    end
 end
