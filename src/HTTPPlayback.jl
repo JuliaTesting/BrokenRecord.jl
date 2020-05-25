0c8446b7595eb1e519a99f8d05daee9c781bddc3
Chris de Graaf <me@cdg.dev>
2020-05-25 11:47:28 -0500

    Add request validation

--- src/HTTPPlayback.jl
+++ src/HTTPPlayback.jl
@@ -1,34 +1,59 @@
 module HTTPPlayback
 
 using BSON: bson, load
-using Cassette: Cassette, disablehooks, overdub, @context
-using HTTP: HTTP, request
+using Cassette: Cassette, overdub, @context
+using HTTP: HTTP, URI, header, request
 using Suppressor: @suppress
 
 function __init__()
     # Hiding the stacktrace from Cassette#174.
-    ctx = RecordingCtx(; metadata=[])
+    ctx = RecordingCtx(; metadata=(; responses=[], save=identity))
     @suppress overdub(ctx, () -> HTTP.get("https://httpbin.org/get"))
 end
 
 @context RecordingCtx
 Cassette.posthook(ctx::RecordingCtx, resp, ::typeof(request), ::Type{Union{}}, args...) =
-    push!(ctx.metadata, resp)
+    push!(ctx.metadata.responses, resp)
 function after(ctx::RecordingCtx, path)
     mkpath(dirname(path))
-    bson(path; responses=ctx.metadata)
+    bson(path; responses=map(ctx.metadata.save, ctx.metadata.responses))
 end
 
 @context PlaybackCtx
-Cassette.overdub(ctx::PlaybackCtx, ::typeof(request), args...) = popfirst!(ctx.metadata)
+function Cassette.prehook(
+    ctx::PlaybackCtx,
+    ::typeof(request),
+    method::Union{AbstractString, Symbol},
+    url, headers, body,
+)
+    isempty(ctx.metadata) && error("No responses remaining in the data file")
+    response = ctx.metadata.load(ctx.metadata.responses[1])
+    request = response.request
+    method == request.method || error("Expected $(request.method) request, got $method")
+    body == request.body || error("Request body does not match")
+    issubset(headers, request.headers) || error("Request headers do not match")
+    observed = URI(url)
+    expected = URI("$(observed.scheme)://$(header(request, "Host"))$(request.target)")
+    observed == expected || error("Expected request to $expected, got $observed")
+end
+function Cassette.overdub(
+    ctx::PlaybackCtx,
+    ::typeof(request),
+    ::Union{AbstractString, Symbol},
+    url, headers, body,
+)
+    return popfirst!(ctx.metadata.responses)
+end
+
 after(ctx::PlaybackCtx, path) = nothing
 
-function playback(f, path; transform=identity)
+function playback(f, path; load_transform=identity, save_transform=identity)
     ctx = if isfile(path)
         data = load(path)
-        disablehooks(PlaybackCtx(; metadata=data[:responses]))
+        metadata = (; responses=data[:responses], load=load_transform, save=save_transform)
+        PlaybackCtx(; metadata=metadata)
     else
-        RecordingCtx(; metadata=[])
+        RecordingCtx(; metadata=(responses=[], save=save_transform))
     end
 
     result = overdub(ctx, f)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using Test: @test, @testset
+using Test: @test, @testset, @test_throws
 
 using BSON: load
 using HTTP: HTTP
@@ -13,10 +13,13 @@ using HTTPPlayback: playback
         @test load(path) == Dict(:responses => [])
 
         path = joinpath(dir, "test2.bson")
-        @test !isfile(path)
         resp1 = playback(() -> HTTP.get("https://httpbin.org/get"), path)
         @test isfile(path)
         resp2 = playback(() -> HTTP.get("https://httpbin.org/get"), path)
         @test resp1.body == resp2.body
+
+        path = joinpath(dir, "test3.bson")
+        playback(() -> HTTP.get("https://httpbin.org/get"), path)
+        @test_throws Exception playback(() -> HTTP.get("https://httpbin.org/ip"), path)
     end
 end
